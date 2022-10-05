ac236e76ffdb434a34c8440053df9e5600b11b31
Aymeric <aymeric.bertrand35@gmail.com>
2022-10-05 22:23:17 +0200

    🚀 Start Project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// https://github.com/vueuse/head
+useHead({
+  title: 'Admin Dashboard',
+  meta: [
+    { name: 'description', content: 'Admin dashboard' },
+    {
+      name: 'theme-color',
+      content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: computed(() => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
+    },
+  ],
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Let's code an admin dashboard.
+  </div>
+</template>
