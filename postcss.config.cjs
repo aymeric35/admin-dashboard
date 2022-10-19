0c688afe54ce60164bd69299685319595c5b7d06
Aymeric <aymeric.bertrand35@gmail.com>
2022-10-19 22:14:14 +0200

    🔧 Create postcss.config.cjs

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+        require('autoprefixer'),
+        require('postcss-mixins'),
+        require('postcss-nesting'),
+        require('postcss-simple-vars')
+    ]
+}
