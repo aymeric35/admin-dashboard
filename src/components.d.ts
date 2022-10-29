b8611dc0b0e97da4030926e666e09de83a8696fd
Aymeric <aymeric.bertrand35@gmail.com>
2022-10-29 21:32:02 +0200

    🔧 Update imports

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,5 +9,9 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    SelectLanguage: typeof import('./components/navigation/SelectLanguage.vue')['default']
+    TheHeader: typeof import('./components/navigation/TheHeader.vue')['default']
+    TheMenu: typeof import('./components/navigation/TheMenu.vue')['default']
+    TheNavigation: typeof import('./components/navigation/TheNavigation.vue')['default']
   }
 }
