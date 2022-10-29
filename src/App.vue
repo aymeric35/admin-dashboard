10ab6a196b3416db5e1e621950fcfe6b5651ea41
Aymeric <aymeric.bertrand35@gmail.com>
2022-10-29 21:25:14 +0200

    🔥 Remove dark theme

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,12 @@ useHead({
   title: 'Admin Dashboard',
   meta: [
     { name: 'description', content: 'Admin dashboard' },
-    {
-      name: 'theme-color',
-      content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
-    },
   ],
   link: [
     {
       rel: 'icon',
       type: 'image/svg+xml',
-      href: computed(() => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
+      href: '/favicon.svg',
     },
   ],
 })
