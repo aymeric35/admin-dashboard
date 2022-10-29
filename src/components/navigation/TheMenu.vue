16f3c951b70c4a19d9365eae2808975769ee5020
Aymeric <aymeric.bertrand35@gmail.com>
2022-10-29 21:31:35 +0200

    ✨ Create TheMenu

---
+++ src/components/navigation/TheMenu.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+const pages = {
+  home: {
+    icon: 'i-mdi:home',
+    translation: 'navigation.home',
+  },
+  profile: {
+    icon: 'i-mdi:card-account-details',
+    translation: 'navigation.profile',
+  },
+  messages: {
+    icon: 'i-mdi:message-reply',
+    translation: 'navigation.messages',
+  },
+  history: {
+    icon: 'i-mdi:history',
+    translation: 'navigation.history',
+  },
+  tasks: {
+    icon: 'i-mdi:file-multiple',
+    translation: 'navigation.tasks',
+  },
+  communities: {
+    icon: 'i-mdi:account-group',
+    translation: 'navigation.communities',
+  },
+  settings: {
+    icon: 'i-mdi:cog',
+    translation: 'navigation.settings',
+  },
+  support: {
+    icon: 'i-mdi:help-circle',
+    translation: 'navigation.support',
+  },
+  privacy: {
+    icon: 'i-mdi:shield-check',
+    translation: 'navigation.privacy',
+  },
+}
+</script> 
+
+<template>
+  <nav class="menu">
+    <ul>
+      <li v-for="page in pages" :key="page.icon">
+        <a class="links">
+          <span :class="page.icon" />
+          {{ $t(page.translation) }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.menu {
+  margin-top: 4rem;
+
+  & ul {
+    display: flex;
+    flex-direction: column;
+    gap: 1.75rem;
+  }
+
+  & .links {
+    display: flex;
+    gap: 0.75rem;
+    justify-items: center;
+  }
+
+  & li:nth-child(7) {
+    margin-top: 4rem;
+  }
+}
+</style>
