d98871ea78a4401a4b0ce6f78b2d903373844e4f
Aymeric <aymeric.bertrand35@gmail.com>
2022-10-30 22:16:35 +0100

    ✨ Add project cards

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    Let's code an admin dashboard.
-  </div>
+  <section>
+    <h2>Your Projects</h2>
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+  </section>
 </template>
+
+<style lang="scss" scoped>
+section {
+  background-color: $neutral-200;
+
+  & h2 {
+    font-size: $font-size-lg;
+    font-weight: 700;
+    margin-bottom: 1rem;
+  }
+}
+</style>
