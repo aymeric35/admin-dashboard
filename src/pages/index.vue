8304f5fffc231c7b0e75765538502063f079f378
Aymeric <aymeric.bertrand35@gmail.com>
2022-11-04 20:32:46 +0100

    🎨 Rework components imports and styles

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,24 +1,38 @@
 <template>
   <section>
     <h2>Your Projects</h2>
-    <ProjectCard />
-    <ProjectCard />
-    <ProjectCard />
-    <ProjectCard />
-    <ProjectCard />
-    <ProjectCard />
-    <ProjectCard />
+    <div class="projects">
+      <TheProject />
+    </div>
+    <h2>Announcements</h2>
+    <div class="announcements">
+      <TheAnnouncement />
+    </div>
+    <h2>Trendings</h2>
+    <div class="trendings" />
   </section>
 </template>
 
 <style lang="scss" scoped>
 section {
   background-color: $neutral-200;
+  padding-bottom: 10vh;
 
-  & h2 {
+  h2 {
     font-size: $font-size-lg;
     font-weight: 700;
     margin-bottom: 1rem;
+  }
+
+  .projects, .announcements {
+    margin-bottom: 5rem;
+  }
+
+  .announcements {
+    @include shadow(md);
+    background-color: $neutral-50;
+    padding: 3rem;
+    border-radius: 0.5rem;
   }
 }
 </style>
