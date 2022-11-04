11c5e3be2eb802a091a7187ee37b2cf0ab2d0b0f
Aymeric <aymeric.bertrand35@gmail.com>
2022-11-04 20:31:19 +0100

    ✨ Create TheProject.vue

---
+++ src/components/home/project/TheProject.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import type { Project } from '~/models/project'
+import ProjectService from '~/services/projectService'
+
+const projects = ref<Project[]>([])
+
+ProjectService.getPosts()
+  .then((response) => {
+    projects.value = response.data.posts
+  })
+  .catch((error) => {
+    return error
+  })
+</script>
+
+<template>
+  <template v-if="projects.length">
+    <ProjectView v-for="n in 10" :key="n" :title="projects[n].title" :description="projects[n].body" />
+  </template>
+</template>
+
