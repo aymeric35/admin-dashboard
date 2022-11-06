9ad52589e16182c06e4119ec0cda433c15ad779c
Aymeric <aymeric.bertrand35@gmail.com>
2022-11-06 11:41:45 +0100

    📱 Improve responsive

--- src/components/home/project/ProjectView.vue
+++ src/components/home/project/ProjectView.vue
@@ -18,8 +18,10 @@ const normalizedText = (length: number, prop: string) => {
 
 <template>
   <div class="card">
-    <h3>{{ normalizedText(25, title).value }}</h3>
-    <p> {{ normalizedText(200, description).value }}</p>
+    <div class="content">
+      <h3>{{ normalizedText(25, title).value }}</h3>
+      <p> {{ normalizedText(200, description).value }}</p>
+    </div>
     <div class="icons">
       <button class="i-mdi:bookmark-outline" />
       <button class="i-mdi:eye-plus-outline" />
@@ -29,40 +31,37 @@ const normalizedText = (length: number, prop: string) => {
 </template>
 
 <style lang="scss" scoped>
-    .card {
-        @include shadow(md);
-        display: flex;
-        flex-direction: column;
-        padding: 3rem 3rem 2rem 3rem;
-        border-radius: 0.5rem;
-        border-left: solid 0.75rem $amber-500;
-        background-color: $neutral-50;
-        margin-bottom: 2.5rem;
+.card {
+  @include shadow(md);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 3rem 3rem 2rem 3rem;
+  border-radius: 0.5rem;
+  border-left: solid 0.75rem $amber-500;
+  background-color: $neutral-50;
 
-        &:last-child {
-          margin-bottom: 0;
-        }
+  h3 {
+    font-size: $font-size-md;
+    font-weight: 700;
+    margin-bottom: 0.5rem;
+  }
 
-        h3 {
-            font-size: $font-size-md;
-            font-weight: 700;
-            margin-bottom: 0.5rem;
-        }
+  p {
+    margin-bottom: 1.75rem;
+  }
 
-        p {
-            margin-bottom: 1.75rem;
-        }
-        .icons {
-            display: flex;
-            justify-content: flex-end;
-            align-items: center;
-            gap: 1.5rem;
-            height: 4rem;
-        }
+  .icons {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 1.5rem;
+    height: 4rem;
+  }
 
-        .icons button {
-            height: 100%;
-            width: 2.5rem;
-        }
-    }
-    </style>
+  .icons button {
+    height: 100%;
+    width: 2.5rem;
+  }
+}
+</style>
