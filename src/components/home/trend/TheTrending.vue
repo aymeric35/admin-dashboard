a32460aa9a764f08365c5d25d8be80719f4c17af
Aymeric <aymeric.bertrand35@gmail.com>
2022-11-06 11:41:28 +0100

    ✨ Add trendings panel

--- src/components/home/trend/TheTrending.vue
+++ src/components/home/trend/TheTrending.vue
@@ -1,3 +1,20 @@
+<script lang="ts" setup>
+const modules = import.meta.glob('~/assets/images/avatar/*.webp')
+const modulesLength = ref<number>(0)
+const avatars = ref<string[]>([])
+const tags = ['@tegan', '@morgan', '@kendall', '@alex']
+const names = ['World Peace Builder', 'Super Cool Project', 'Life Changing App', 'No Traffic Maker']
+
+for (const path in modules) {
+  modules[path]().then(() => {
+    avatars.value.push(path)
+  })
+  if (modulesLength.value === 0) modulesLength.value = Object.keys(modules).length
+}
+</script>
+
 <template>
-  <div class="trends" />
+  <template v-if="avatars.length === modulesLength">
+    <TrendingView v-for="(_, i) in 4" :key="i" :avatar="avatars[i]" :tag="tags[i]" :name="names[i]" />
+  </template>
 </template>
---
+++ src/components/home/trend/TrendingView.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+defineProps({
+  avatar: {
+    type: String,
+    required: true,
+  },
+  tag: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="trend">
+    <img :src="avatar">
+    <h3><span>{{ tag }}</span> {{ name }}</h3>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.trend {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+
+    img {
+        width: 8rem;
+        border-radius: 50%;
+    }
+
+    span {
+        color: $neutral-800;
+    }
+
+    h3 {
+        display: flex;
+        flex-direction: column;
+        font-size: $font-size-md;
+        color: $neutral-600;
+    }
+}
+
+.trend:not(:first-child) {
+    margin-top: 2rem;
+}
+
+.trend:not(:last-child) {
+    padding-bottom: 3rem;
+}
+</style>
