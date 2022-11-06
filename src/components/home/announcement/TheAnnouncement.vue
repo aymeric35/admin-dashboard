0d1a883c7bc00bdd1922a8f6b8f871e9e7de5016
Aymeric <aymeric.bertrand35@gmail.com>
2022-11-06 11:42:50 +0100

    💄 Add styles to component

--- src/components/home/announcement/TheAnnouncement.vue
+++ src/components/home/announcement/TheAnnouncement.vue
@@ -18,27 +18,3 @@ AnnouncementService.getAnnouncements()
     <AnnouncementView v-for="n in 3" :key="n" :title="announcements[n].title" :description="announcements[n].body" />
   </template>
 </template>
-
-<style lang="scss" scoped>
-.announce {
-  h3 {
-    font-weight: 700;
-    font-size: $font-size-md;
-    padding-bottom: 0.75rem;
-  }
-
-  p {
-    color: $neutral-600;
-  }
-}
-
-.announce:not(:first-child) {
-  margin-top: 2rem;
-}
-
-.announce:not(:last-child) {
-  border-bottom: solid 0.1rem rgba($color: $neutral-900, $alpha: 0.2);
-  padding-bottom: 3rem;
-}
-</style>
-
