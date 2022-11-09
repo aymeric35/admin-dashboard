73d10763c4070b57360fc959d56a3cc7d1e04d2a
Aymeric <aymeric.bertrand35@gmail.com>
2022-11-09 20:55:42 +0100

    ✨ Create WIP component

---
+++ src/components/WorkInProgress.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="page-container">
+    <h1>{{ $t('WIP.title') }}</h1>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    background-color: $amber-100;
+    h1 {
+        font-size: $font-size-xxl;
+    }
+}
+</style>
